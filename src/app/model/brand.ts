36a6fdd2380a69ab7863464aeeb65334d082ebc8
Manju_rgc <manjulakshmi@rgcdynamics.com>
2021-11-10 04:38:53 +0530

    first commit

---
+++ src/app/model/brand.ts
@@ -0,0 +1,8 @@
+export class Brand {
+    src!:string
+    name!:string
+    constructor(src:string,name:string){
+      this.src=src
+      this.name=name
+    }
+}
---
+++ src/app/service/model.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Brand } from '../model/brand'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModelService {
+
+  constructor() { }
+
+}
